4b429f2509da30816434b5e9376461fccc258250
aditya199746 <68444836+aditya199746@users.noreply.github.com>
2024-05-09 12:37:26 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,133 +1,131 @@
 
-// Promise.myAll=function(promises){
-//     console.log("111")
-//     return new Promise((resolve,reject)=>{
-//         if(!promises.length){
-//             resolve([])
-//         }
-//         let poolResponse=[]
-//         let count=0
-//         promises.forEach(promise => {
-//             Promise.resolve(promise)
-//             .then((res)=>{
-//                 poolResponse[count]=res
-//                 count++
-//                 console.log(count,"count")
-//                 if(count===promises.length.length)
-//                 {
-//                     resolve(poolResponse)
-//                 }
-//             })
-//             .catch((err)=>{
-//                 console.log("22")
-//                 reject(err)
-//             })
-//         });
-//     })
+ Promise.myAll=function(promises){
+     console.log("111")
+     return new Promise((resolve,reject)=>{
+         if(!promises.length){
+             resolve([])
+         }
+         let poolResponse=[]
+         let count=0
+        promises.forEach(promise => {
+            Promise.resolve(promise)
+            .then((res)=>{
+                     poolResponse[count]=res
+                 count++
+                 console.log(count,"count")
+                     if(count===promises.length.length)
+                 {
+                     resolve(poolResponse)
+                 }
+             })
+             .catch((err)=>{
+                 console.log("22")
+                 reject(err)
+             })
+         });
+     })
     
-// }
+ }
 
 
 
-// let arr=[
-//     new Promise(res=>setTimeout(res,10,"apple")),
-//     new Promise(res=>setTimeout(res,20,"banana")),
-//     new Promise((res,rej)=>setTimeout(rej,30,"orange")),
-//     new Promise(res=>setTimeout(res,3000,"mango")),
-// ]
+ let arr=[
+     new Promise(res=>setTimeout(res,10,"apple")),
+     new Promise(res=>setTimeout(res,20,"banana")),
+     new Promise((res,rej)=>setTimeout(rej,30,"orange")),
+     new Promise(res=>setTimeout(res,3000,"mango")),
+ ]
 
-// let result=Promise.myAll(arr)
-// .then((res)=>console.log(res,"result"))
-// .catch((err)=>console.log(err,"error"))
+ let result=Promise.myAll(arr)
+ .then((res)=>console.log(res,"result"))
+ .catch((err)=>console.log(err,"error"))
 
-// Promise.allSettled=function(promises){
-//     console.log("settled")
-//     return new Promise((resolve,reject)=>{
-//         if(!promises.length)
-//         {
-//             this.resolve([])
-//         }
+ Promise.allSettled=function(promises){
+     console.log("settled")
+     return new Promise((resolve,reject)=>{
+         if(!promises.length)
+         {
+             this.resolve([])
+         }
 
-//         let poolResponse=[]
-//         let count=0
-//         promises.forEach(promise => {
-//             Promise.resolve(promise)
-//             .then((res)=>{
-//                 poolResponse[count]={
-//                     status:"fullfilled",
-//                     value:res
-//                 }
-//             })
-//             .catch((err)=>{
-//                 poolResponse[count]={
-//                     status:"rejected",
-//                     value:err
-//                 }
-//             })
-//             .finally(()=>{
-//                 count++
-//                 if(count===promises.length)
-//                 {
-//                     resolve(poolResponse)
-//                 }
-//             })
-//         });
-
-//     })
-    
-// }
+         let poolResponse=[]
+        let count=0
+         promises.forEach(promise => {
+             Promise.resolve(promise)
+             .then((res)=>{
+                 poolResponse[count]={
+                     status:"fullfilled",
+                     value:res
+                 }
+             })
+            .catch((err)=>{
+                 poolResponse[count]={
+                     status:"rejected",
+                     value:err
+                }
+            })
+             .finally(()=>{
+                 count++
+                if(count===promises.length)
+                 {
+                    resolve(poolResponse)
+                 }
+             })
+       });
+     })   
+}
 
 
-// let result=Promise.allSettled(arr)
-// .then((res)=>console.log(res,"Result"))
-// .catch((err)=>console.log(err,"error"))
+ let result=Promise.allSettled(arr)
+ .then((res)=>console.log(res,"Result"))
+ .catch((err)=>console.log(err,"error"))
 
-// Array.prototype.map=function(callBack){
-//     let res=[]
-//     for(let i=0;i<this.length;i++)
-//     {
-//         res.push(callBack(i,this[i],this))
-//     }
-//     return res
-// }
+ Array.prototype.map=function(callBack){
+     let res=[]
+     for(let i=0;i<this.length;i++)
+     {
+         res.push(callBack(i,this[i],this))
+     }
+     return res
+ }
 
-// Array.prototype.filter=function(callBack){
-//     console.log("1111")
-//     let poolResponse=[]
-//     for(let i=0;i<this.length;i++)
-//     {
-//         if(callBack(this[i],i,this))
-//         {
-//             poolResponse.push(this[i])
-//         }
-//     }
-//     return poolResponse
-// }
+ Array.prototype.filter=function(callBack){
+     console.log("1111")
+     let poolResponse=[]
+     for(let i=0;i<this.length;i++)
+     {
+         if(callBack(this[i],i,this))
+         {
+             poolResponse.push(this[i])
+         }
+     }
+     return poolResponse
+ }
 
-// let arr=[1,2,3,4,5,6]
+let arr=[1,2,3,4,5,6]
 
-// console.log(arr.filter((a)=>a%2===0))
-
-
-// Array.prototype.reduce=function(callBack,initialVal){
-//     let acc=initialVal
-//     for(let i=0;i<this.length;i++)
-//     {
-//         if(acc!==undefined)
-//         {
-//             acc=callBack(acc,initialVal,this,this[i])
-//         }
-//         else{
-//             acc=this[i]
-//         }
-//     }
-//     return acc
-// }
+console.log(arr.filter((a)=>a%2===0))
 
 
-// let arr=[1,2,3,4]
+ Array.prototype.reduce=function(callBack,initialVal){
+     let acc=initialVal
+     for(let i=0;i<this.length;i++)
+     {
+        if(acc!==undefined)
+        {
+             acc=callBack(acc,initialVal,this,this[i])
+         }
+         else{
+             acc=this[i]
+         }
+    }
+     return acc
+ }
 
-// console.log(arr.reduce((acc,val)=>acc+=val,0))
+
+ let arr=[1,2,3,4]
+
+ console.log(arr.reduce((acc,val)=>acc+=val,0))
 
 function flattenArray(arr, depth = Infinity) {
     const result = [];
